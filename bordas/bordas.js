46ad85190325ad1ff8e2542432ac1c70ccb9058a
Augusto Seabra <augusto.t.seabra@hotmail.com>
2020-07-15 10:21:47 -0300

    Subindo capitulo bordas

---
+++ bordas/bordas.js
@@ -0,0 +1,6 @@
+const texto = 'Romário era um excelente jogador\n, mas hoje é um político questionador'
+console.log(texto.match(/r/gi))
+console.log(texto.match(/^r/gi)) // início da linha/string
+console.log(texto.match(/r$/gi)) // fim da linha/string
+
+console.log(texto.match(/^r.*r$/gi)) // Problema do dotall (o ponto, dessa forma, não resolve o \n)
